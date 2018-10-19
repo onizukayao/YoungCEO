0a42aed6f484696d725db8821bb9ed6c511c133a
sylt <yaohf11@chinaunicom.cn>
2018-10-19 09:29:13 +0800

    first commit yhf

---
+++ YoungCEO/src/com/mybatis/util/MybatisUtil.java
@@ -0,0 +1,43 @@
+package com.mybatis.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * @author YaoHF
+ * 2018年9月26日 - 下午2:45:19
+ * description:
+ */
+public class MybatisUtil {
+	private MybatisUtil(){}
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	/**
+	 * 通过单例模式创建SqlSessionFactory
+	 */
+	private static SqlSessionFactory getsqlSessionFactory(){
+		//String configPath="config.xml";
+		String configPath="sqlMapConfig.xml";
+		try {
+			InputStream inputStream=Resources.getResourceAsStream(configPath);
+			
+			if(sqlSessionFactory==null){
+				sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sqlSessionFactory;
+	}
+	/**
+	 * 创建session实例
+	 */
+	public static SqlSession getSqlSession(){
+		return MybatisUtil.getsqlSessionFactory().openSession();
+	}
+}
---
+++ YoungCEO/src/com/youngceo/test/GeneratorSqlmap.java
@@ -0,0 +1,42 @@
+package com.youngceo.test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ * @author YaoHF
+ * 2018年9月28日 - 下午8:56:36
+ * description:
+ */
+public class GeneratorSqlmap {
+	public void generator() throws Exception{
+
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指定 逆向工程配置文件
+		File configFile = new File("src/generatorConfig.xml"); 
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+
+	} 
+	public static void main(String[] args) throws Exception {
+		try {
+			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+			generatorSqlmap.generator();
+			//System.out.println("1111");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
